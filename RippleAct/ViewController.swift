41682a339e21ddb247680d101574523a258b1737
lorraine <lmcgonigle@vectra.ai>
2018-12-12 22:01:23 +0000

    initial commit

--- RippleAct/ViewController.swift
+++ RippleAct/ViewController.swift
@@ -2,18 +2,34 @@
 //  ViewController.swift
 //  RippleAct
 //
-//  Created by loaner on 12/12/18.
+//  Created by Lorraine on 12/12/18.
 //  Copyright © 2018 Ripple Act. All rights reserved.
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.rippleact.com")!
+        webView.load(URLRequest(url: url))
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
     }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
 
 
 }
